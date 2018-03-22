f8fbfb440e2b7e618bf2aaf4dd3a3dbe8ed07561
Koen Pijnenburg <kpapijnenburg@gmail.com>
2018-03-22 09:41:54 +0100

    Added bootstrap

--- scripts/code.js
+++ scripts/code.js
@@ -1,24 +1,16 @@
 function OnReady() {
     console.log('Pagina geladen, DOM klaar voor gebruik.');
-    
+
+    $("#naam").focus(function () {
+        $('#hidden').show();
+    });
+
+    $("#naam").blur(function () {
+        $('#hidden').hide();
+    });
 }
 
 $(document).ready(OnReady);
-
-function hide() {
-    var element = document.getElementById('hidden');
-    element.style.visibility = 'hidden';
-    console.log("unfocus")
-}
-
-function show() {
-    var element = document.getElementById('hidden');
-    element.style.visibility = 'visible';
-    console.log("focus")
-}
-
-$("naam").focus(show());
-$("naam").blur(hide());
 
 
 
